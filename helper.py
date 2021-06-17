6f99c7e5a8c5e879855909dd43d14b9dbc6510da
john so <johnianrso@berkeley.edu>
2021-06-16 18:20:46 -0700

    asst 1 checkpoint

--- helper.py
+++ helper.py
@@ -10,7 +10,12 @@ class Model(ABC):
     See the forward method for more information.
     """
 
-    def __init__(self, act_dim, obs_dim, sigma=1, seed=0):
+    def __init__(self,
+                 act_dim,
+                 obs_dim,
+                 initial_state=None,
+                 sigma=1,
+                 seed=0):
         """
         bottom text
         """
@@ -18,7 +23,7 @@ class Model(ABC):
         self.act_dim = act_dim
         self.obs_dim = obs_dim
         self.sigma = sigma
-        self._state = np.random.random(self.obs_dim)
+        self._state = np.random.random(self.obs_dim) if initial_state is None else initial_state
 
     @abstractmethod
     def forward(self, input=None, state=None):
@@ -70,3 +75,31 @@ class Complex1DModel(Model):
 
 def MSE_loss(x, y):
     return np.mean((x - y).T @ (x - y))
+
+class Motion2DModel(Model):
+    A = np.array([[1, .037, 0, 0],
+                  [0, .63, 0, 0],
+                  [0, 0, 1, .037],
+                  [0, 0, 0, .63]])
+
+    b = np.array([[0, 0],
+                  [.24, 0],
+                  [0, 0],
+                  [0, .24]])
+
+    def forward(self, input=None, state=None):
+        if state is None:
+            state_0 = self._state
+        else:
+            state_0 = state
+
+        if input is None:
+            input = np.zeros(self.act_dim)
+
+        state_1 = self.A @ state_0
+        state_1 = state_1 + (self.b @ input)
+        state_1 = state_1 + np.random.normal(0, self.sigma, self.obs_dim)
+
+        if state is None:
+            self._state = state_1
+        return state_0, state_1
